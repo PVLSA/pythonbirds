bb1120e5a654bc181e1df9f1a378ac4c881e22ca
PAULO <pvlsa1977@gmail.com>
2021-03-17 18:32:16 -0300

    Corrigida a identação

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -2,8 +2,9 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
-    if __name__ == '__main__':
-        p=Pessoa()
-        print(Pessoa.cumprimentar(p))
-        print(id(p))
-        print(p.cumprimentar())
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+
